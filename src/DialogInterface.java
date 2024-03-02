30bb38e5dcc299753faebafc9f70ab7d65eabb17
Shaizam2003 <119006164+Shaizam2003@users.noreply.github.com>
2024-03-02 23:22:41 +0000

    Add files via upload

---
+++ src/DialogInterface.java
@@ -0,0 +1,37 @@
+
+
+	import javax.swing.JDialog;
+	import java.util.Vector;
+
+	// Abstract factory interface for dialog creation
+	public interface DialogInterface {
+		
+	    JDialog createEmployeeSummaryDialog(EmployeeDetails parent, Vector<Object> allEmployees);
+	    JDialog createSearchByIdDialog(EmployeeDetails parent);
+	    JDialog createSearchBySurnameDialog(EmployeeDetails parent);
+	    JDialog createAddRecordDialog(EmployeeDetails parent, ClearFields clearFields, ManagerRecord Managerrecord);
+	}
+
+	// Concrete factory implementing the DialogFactory interface
+	class ConcreteDialogFactory implements DialogInterface {
+	    @Override
+	    public JDialog createEmployeeSummaryDialog(EmployeeDetails parent, Vector<Object> allEmployees) {
+	        return new EmployeeSummaryDialog(parent, allEmployees);
+	    }
+
+	    @Override
+	    public JDialog createSearchByIdDialog(EmployeeDetails parent) {
+	        return new SearchByIdDialog(parent);
+	    }
+
+	    @Override
+	    public JDialog createSearchBySurnameDialog(EmployeeDetails parent) {
+	        return new SearchBySurnameDialog(parent);
+	    }
+
+	    @Override
+	    public JDialog createAddRecordDialog(EmployeeDetails parent, ClearFields clearFields, ManagerRecord Managerrecord) {
+	        return new AddRecordDialog(parent, clearFields, Managerrecord);
+	    }
+	}
+
